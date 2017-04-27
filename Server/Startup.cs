c344f5b360e0c6578324450194b1e144f314cb5b
Lars Møllebjerg <lars@moellebjerg.com>
2017-04-27 06:38:47 +0200

    Enable response compression using middleware.

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Linq;
 
 namespace KiCadDoxer
 {
@@ -17,15 +19,22 @@ namespace KiCadDoxer
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseResponseCompression();
+
+            
             app.MapWhen(
                 context => SchematicRenderSettingsHttpRequest.CanHandleContext(context),
                 appBranch => appBranch.UseSchematicMiddleware());
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddResponseCompression(options =>
+            {
+                // We do not return query parameters in the response, so it should not be possible to use the compression to attack the SSL.
+                options.EnableForHttps = true;
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "image/svg+xml" }).ToArray();
+            });
         }
     }
 }
